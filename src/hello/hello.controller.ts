d66f8e9cf9c5c4101d0ea574fa893b1fbba650bf
Carlos Vega Oyola <carlos.vega@instipp.edu.ec>
2026-01-31 17:55:22 -0500

    httpcode para gestionar estados http

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import type { Request, Response } from 'express';
 
 @Controller()
@@ -9,5 +9,23 @@ export class HelloController {
     response.status(200).json({
       message: 'Hello, World!',
     });
+  }
+
+  @Get('new')
+  @HttpCode(201)
+  somethingNew(){
+    return 'This is something new!';
+  }
+
+  @Get('not-found')
+  @HttpCode(404)
+  notFoundPage() {
+    return '404 not found';
+  }
+
+  @Get('error')
+  @HttpCode(500)
+  errorPage() {
+    return 'Error route';
   }
 }
